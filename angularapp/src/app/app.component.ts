1860a91a292dd28a7d71875b82ce732ee1dbd831
Iamneo <techadmin@iamneo.ai>
2023-08-02 09:18:00 +0000

    Backup Commit

---
+++ angularapp/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+    templateUrl: './app.component.html',
+      styleUrls: ['./app.component.css']
+      })
+      export class AppComponent {
+        title = 'CaloryTracker';
+          isShowForm = true;
+            
+              public data:any[]=[];
+
+                public qData : any = '';  
+
+                  public cData : number = 0.0;
+
+                    public fData : any = '';
+
+                      searchText = '';
+
+                        onSearchTextEntered(searchVal:string){
+                            this.searchText = searchVal;
+                                
+                                  }
+
+                                    toggleFormDiv(){
+                                        console.log("clicked")
+                                            this.isShowForm = !this.isShowForm;
+                                                console.log(this.qData)
+                                                  }
+                                                  }
+                                                  
---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { FormComponent } from './form/form.component';
+import { AppFoodListComponent } from './app-food-list/app-food-list.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+      AppComponent,
+          NavbarComponent,
+              SearchbarComponent,
+                  FormComponent,
+                      AppFoodListComponent,
+                        
+                          ],
+                            imports: [
+                                BrowserModule,
+                                    AppRoutingModule,
+                                        FormsModule
+                                          ],
+                                            providers: [],
+                                              bootstrap: [AppComponent]
+                                              })
+                                              export class AppModule { }
+                                              
